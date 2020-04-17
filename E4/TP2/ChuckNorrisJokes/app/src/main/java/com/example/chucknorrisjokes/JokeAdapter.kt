2fdcedfc26fa44787d11849bcf4de0980fe10597
Neoxes <neoxes38@gmail.com>
2020-04-17 18:30:39 +0200

    Deleted useless things

--- E4/TP2/ChuckNorrisJokes/app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
+++ E4/TP2/ChuckNorrisJokes/app/src/main/java/com/example/chucknorrisjokes/JokeAdapter.kt
@@ -1,30 +1,94 @@
 package com.example.chucknorrisjokes
 
-import android.view.LayoutInflater
+import android.content.Intent
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 
-class JokeAdapter(var jokes : MutableList<Joke>, val onBottomReached: (JokeAdapter) -> Unit?) : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
+class JokeAdapter(var jokes: MutableList<Joke>, val onBottomReached: (JokeAdapter) -> Unit?): RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
 
-    class JokeViewHolder(val jokes_view : TextView) : RecyclerView.ViewHolder(jokes_view)
+    class JokeViewHolder(val jokeView: JokeView): RecyclerView.ViewHolder(jokeView)
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
-            // Inflate the layout using LayoutInflater
-            val textView = LayoutInflater.from(parent.context).inflate(R.layout.joke_layout, parent,false) as TextView
+    // Shared jokes logs
+    var logsShare: MutableList<String> = mutableListOf()
+    // Fav jokes logs
+    var logsStar: MutableList<String> = mutableListOf()
 
-            return JokeViewHolder(textView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
+        val jokeViewCreated = JokeView(parent.context)
+
+        // Inflate in JokeView doesn't take account of parent so we use LayoutParams
+        val lp = RecyclerView.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        jokeViewCreated.layoutParams = lp
+
+        return JokeViewHolder(jokeViewCreated)
+    }
+
+    override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
+        if (position == this.itemCount - 1) {
+            onBottomReached(this)
         }
 
-        override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
-            holder.jokes_view.text = jokes[position].value
-        }
+        // We create a TextView with the new Joke
+        val newTView = holder.jokeView.findViewById(R.id.joke_view) as TextView
+        newTView.text = jokes[position].value
 
-        override fun getItemCount(): Int {
-            return jokes.size
-        }
+        // Behavior of the fav button
+        val newFavB = holder.jokeView.findViewById(R.id.fav_button) as ImageButton
+        newFavB.setOnClickListener(object : View.OnClickListener{
 
-    fun addJoke(joke : Joke){
+            override fun onClick(v: View) // When we click on the fav button
+            {
+                if (logsStar.contains(jokes[position].id)) // If the Joke has already been added to the list of favorite jokes
+                {
+                    logsStar.remove(jokes[position].id) // Remove the Joke from the list of favorite jokes
+                    newFavB.setImageResource(R.drawable.fav_star) // Change the image to fav_star (default image for the fav button)
+                }
+
+                else // If the Joke is not in the list of favorite jokes
+                {
+                    logsStar.add(jokes[position].id) // Add the Joke's id to logs_star list
+                    newFavB.setImageResource(R.drawable.clicked_fav_star) // Change the image to clicked_fav_star (image for when the button has been clicked)
+
+                }
+            }
+        })
+
+        // Behavior of the share button
+        val newShareB = holder.jokeView.findViewById(R.id.share_button) as ImageButton
+        newShareB.setOnClickListener(object: View.OnClickListener {
+
+            override fun onClick(v: View) // When we click on the share button
+            {
+                logsShare.add(jokes[position].id) // Add the Joke's id to logs_share list
+
+                // Share of the joke
+                val sendIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, jokes[position].value) //text to sent
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(sendIntent, "Share" )
+                startActivity(holder.jokeView.context,shareIntent,null)
+            }
+        })
+
+        val newModel = JokeView.Model(newTView, newShareB, newFavB)
+
+        holder.jokeView.setupView(newModel) // We update the view
+    }
+
+    override fun getItemCount(): Int {
+        return jokes.count()
+    }
+
+    fun addJoke(joke: Joke){
         this.jokes.add(joke)
         this.notifyDataSetChanged()
     }
